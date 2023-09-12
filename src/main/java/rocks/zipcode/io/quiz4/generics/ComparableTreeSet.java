3d83cb1622cdc6e02170a75d0f07e16907f96595
maggie <snydermag@gmail.com>
2023-09-12 11:24:32 -0400

    some of the power sets are populated.

--- src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
@@ -1,17 +1,30 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class    ComparableTreeSet<_> {
-    public ComparableTreeSet(_... arr) {
+public class ComparableTreeSet<T> implements Comparable<T>{
+    Set<T> set;
+    public ComparableTreeSet(T... arr) {
+        set = new TreeSet<>();
+        for (T t : arr){
+            set.add(t);
+        }
     }
-
 
     public ComparableTreeSet() {
+        set = new TreeSet<>();
     }
 
-    public int compareTo(ComparableTreeSet<_> o) {
+    public int compareTo(ComparableTreeSet<T> o) {
         return Integer.valueOf(null);
+    }
+
+    @Override
+    public int compareTo(T o) {
+        return 0;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -1,18 +1,39 @@
 package rocks.zipcode.io.quiz4.generics;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class GenericUtils {
-    public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(Set<_> originalSet) {
-        return null;
+    public static <T extends Comparable> Iterable<? extends Iterable<T>> powerSet(Set<T> originalSet) {
+        Set<Set<T>> set = new LinkedHashSet<>();
+        List<List<T>> list = new ArrayList<>();
+        Object[] array = originalSet.toArray();
+
+        for (int i = 0; i < originalSet.size(); i++){
+            List<T> temp = new ArrayList<>();
+            for (int j = i; j < originalSet.size(); j++){
+                temp.add((T) array[j]);
+            }
+            list.add(temp);
+
+            List<T> other = new ArrayList<>();
+            for (int j = i + 1; j < originalSet.size(); j++){
+                other.add((T) array[i]);
+                other.add((T) array[j]);
+                list.add(other);
+            }
+
+        }
+        List<T> blank = new ArrayList<>();
+
+        list.add(blank);
+
+        return list;
     }
 
-    public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(_... originalSet) {
+    public static <T extends Comparable> Iterable<? extends Iterable<T>> powerSet(T... originalSet) {
         return powerSet(new TreeSet<>(Arrays.asList(originalSet)));
     }
 }
